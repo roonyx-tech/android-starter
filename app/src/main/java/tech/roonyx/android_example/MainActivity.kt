710691c3dacc5b6b49ca9128403f228e68fdd3ca
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.tech>
2021-10-11 15:49:47 +0300

    🚀 Create Android project

---
+++ app/src/main/java/tech/roonyx/android_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package tech.roonyx.android_example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tech.roonyx.android_example.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/tech/roonyx/android_example/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package tech.roonyx.android_example.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
